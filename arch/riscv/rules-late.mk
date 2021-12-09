325de5e9229dcaf559708bdbfdbcb0b5fda09b04
Alex Richardson <alexrichardson@google.com>
2021-12-09 12:13:30 +0000

    [riscv][clang] Use -fdirect-access-external-data when available
    Building with -fPIE adds unncessary GOT indirections for data symbols.
    Avoid those by building with -fdirect-access-external-data when using
    the -fPIE workaround.

--- arch/riscv/rules-late.mk
+++ arch/riscv/rules-late.mk
@@ -12,11 +12,15 @@ endif
 # See https://github.com/riscv-non-isa/riscv-elf-psabi-doc/issues/126 for more
 # details. For now, the simplest workaround is to build with -fpie when using
 # a version of clang that does not include https://reviews.llvm.org/D107280.
+check_compiler_flag = $(shell $(CC) -c -xc /dev/null -o /dev/null $(1) 2>/dev/null && echo yes || echo no)
 ifeq ($(LINKER_TYPE),lld)
 # TODO: Add a clang version check once https://reviews.llvm.org/D107280 lands.
 ifeq ($(COMPILER_TYPE),clang)
-# TODO: Clang 12+ has -fdirect-access-external-data to avoid the majority of
-# the performance overhead caused by -fPIE
 ARCH_COMPILEFLAGS += -fPIE
+_have_direct_access := $(call check_compiler_flag,-fdirect-access-external-data)
+$(info -fdirect-access-external-data supported = $(_have_direct_access))
+ifeq ($(_have_direct_access),yes)
+ARCH_COMPILEFLAGS += -fdirect-access-external-data
+endif
 endif
 endif
